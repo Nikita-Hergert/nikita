12176cd8f5957a07f9a7a83bd3610b74ab330089
Vitali <vitali.dettling@gmail.com>
2023-05-23 11:32:21 +0000

    introduce test framework mocha

---
+++ test/app/vcap_application.test.js
@@ -0,0 +1,12 @@
+// var assert = require('assert');
+const expect = require('chai').expect;
+var app = require('../../src/app/vcap_application');
+
+describe('Beck End (BE) Tests', function () {
+  // Timeout to gibe the test a bit more time to finish
+  this.timeout(10000);
+    it('Your very first test', function () {
+      const appappName = app.get_app_name();
+      expect(appappName).to.be.undefined;
+    });
+});
